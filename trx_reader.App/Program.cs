8e2fcf127f6a5965a918c211f7c6df251b4049c6
Matthew <matthewtlittle@gmail.com>
2026-01-07 17:16:43 +1000

    example API call added

---
+++ trx_reader.App/Program.cs
@@ -0,0 +1,28 @@
+﻿using Anthropic;
+using Anthropic.Models.Messages;
+
+class trx_reader
+{
+    static async Task Main()
+    {
+        var client = new AnthropicClient();
+
+        MessageCreateParams parameters = new()
+        {
+            MaxTokens = 1024,
+            Messages = [new() { Role = Role.User, Content = "Hello, Claude" }],
+            Model = "claude-sonnet-4-5",
+        };
+        var response = await client.Messages.Create(parameters);
+
+        var message = string.Join(
+            "",
+            response
+                .Content.Where(message => message.Value is TextBlock)
+                .Select(message => message.Value as TextBlock)
+                .Select((textBlock) => textBlock.Text)
+        );
+
+        Console.WriteLine(message);
+    }
+}
